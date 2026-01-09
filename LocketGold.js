71d41ce470179a42e200620a5270957482429ea4
NgDanhThanhTrung <thanhtrungnguyendanh@gmail.com>
2026-01-09 18:55:43 +0600

    Update LocketGold.js

--- LocketGold.js
+++ LocketGold.js
@@ -1,6 +1,6 @@
 const mapping = {
   '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip', 'watch_vip'],
-  'Locket': ['Gold', 'com.is3l5.8.premium.yearly']
+  'Locket': ['Gold', 'com.ttrungisme.bi.premium.yearly']
 };
 
 const ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
@@ -24,7 +24,7 @@ const premiumInfo = {
 const entitlementInfo = {
   grace_period_expires_date: null,
   purchase_date: "2008-03-05T01:04:17Z",
-  product_identifier: "com.is3l5.8.premium.yearly",
+  product_identifier: "com.ttrungisme.bi.premium.yearly",
   expires_date: "2999-12-18T01:04:17Z"
 };
 
@@ -32,13 +32,13 @@ const match = Object.keys(mapping).find(e => ua.includes(e));
 
 if (match) {
   let [entKey, subKey] = mapping[match];
-  let finalSubKey = subKey || "com.is3l5.8.premium.yearly";
+  let finalSubKey = subKey || "com.ttrungisme.bi.premium.yearly";
   
   entitlementInfo.product_identifier = finalSubKey;
   obj.subscriber.subscriptions[finalSubKey] = premiumInfo;
   obj.subscriber.entitlements[entKey] = entitlementInfo;
 } else {
-  obj.subscriber.subscriptions["com.is3l5.8.premium.yearly"] = premiumInfo;
+  obj.subscriber.subscriptions["com.ttrungisme.bi.premium.yearly"] = premiumInfo;
   obj.subscriber.entitlements["Gold"] = entitlementInfo;
   obj.subscriber.entitlements["pro"] = entitlementInfo;
 }
